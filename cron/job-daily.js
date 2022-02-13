ba73551ec87798fc25998c1eb8eac9249f2421dc
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-02-14 00:47:28 +0200

    commit

--- cron/job-daily.js
+++ cron/job-daily.js
@@ -13,6 +13,7 @@ module.exports = function jobDaily () {
         User.find()
             .then((response)=>{
                 response.map((el)=>{
+                    console.log('el.cronTime', el);
                     const job = new CronJob(el.cronTime, async function() {
                         console.log('CronJob: email', el.email);
                         new BigCommerce({
